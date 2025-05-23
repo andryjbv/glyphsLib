d3d62a8b47373d5ef006b8c3f1bb216638bc53cf
Andry Becerra <andryjbv@gmail.com>
2025-05-22 22:50:11 -0400

    Add Dockerfile and build/test scripts

--- build.sh
+++ build.sh
@@ -9,14 +9,17 @@ set -e
 ###############################################
 # TODO: Install project dependencies if needed based on relevant config/lock files in the repo.
 # Note that we are developing the project, even if dependencies have been installed before, we need to install again to accommodate the changes we made.
-# <dependency-installation-commands>
+# Install runtime and development dependencies
+pip install --no-cache-dir -r requirements.txt -r requirements-dev.txt
+# Install the package in editable mode for testing
+pip install --no-cache-dir -e .
 # TODO: Configure project and environment variables
-# <config-commands>
+# No additional configuration required for this project
 
 ###############################################
 # BUILD
 ###############################################
 echo "================= 0909 BUILD START 0909 ================="
 # TODO: Build the project if needed. Note that we are developing the project and making changes to it, even if it has been build before, we need to build it again.
-# <build-commands>
+# Nothing to compile for this pure Python project
 echo "================= 0909 BUILD END 0909 ================="
--- run.sh
+++ run.sh
@@ -6,18 +6,14 @@ set -e
 # Replace this with your command to run all tests
 run_all_tests() {
   echo "Running all tests..."
-  # Example: pytest tests/
-  # TODO: Replace with your command to run all tests
-  # Your command here
+  pytest -v tests/
 }
 
 # Replace this with your command to run specific test files
 run_selected_tests() {
   local test_files=("$@")
   echo "Running selected tests: ${test_files[@]}"
-  # Example: pytest "${test_files[@]}"
-  # TODO: Replace with your command to run specific test files
-  # <Your command here>
+  pytest -v "${test_files[@]}"
 }
 # --- END CONFIGURATION SECTION ---
 
@@ -39,3 +35,4 @@ fi
 
 # Run them all together
 run_selected_tests "${TEST_FILES[@]}"
+
